2e1c5951f0f59ae6f3a2b57ca30cdec873febb7f
Antonio <alu-antmorjim@foc.es>
2025-01-15 18:08:40 +0100

    ejercicio Clase empleado

---
+++ PCAP/3.OOP/ejEmpleado.py
@@ -0,0 +1,33 @@
+'''
+Crear una clase Empleado que modele trabajadores con un nombre y apellidos, un cargo y un salario. El salario debe ser (obligatoriamente) un atributo privado.
+'''
+
+class Empleado:
+    def __init__(self, nombre, apellidos, cargo, salario=1000):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.cargo = cargo
+        self.__salario=salario
+
+    def getSalario(self):
+        return self.__salario
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellidos} - ({self.cargo}) - Gana:  {round(self.getSalario())}'
+    
+listaEmpleados =[
+    Empleado('Juan', 'Perez', 'Gerente', 75000.0),
+    Empleado('Ana', 'Garcia', 'Secretaria', 80000.0),
+    Empleado('Pedro', 'Lopez', 'Programador', 2800.0),
+    Empleado('Mario', 'Rodriguez', 'Conserge', 20000.0)
+]
+
+empelados_vip=[
+    empleado for empleado in listaEmpleados
+    if empleado.getSalario() > 50000.0
+]
+
+for e in empelados_vip:
+    print(e)
+
+    
